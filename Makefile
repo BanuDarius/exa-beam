45ebcc263953dbd2a213b5a432630a0c5560e933
BanuDarius <darius-matei.banu@s.unibuc.ro>
2026-06-05 16:53:19 +0300

    Added makefile

---
+++ Makefile
@@ -0,0 +1,47 @@
+CC = g++
+OPT_FLAG = -O3
+WARNINGS = -Wall -Wextra -Wshadow
+CFLAGS = $(OPT_FLAG) -march=native -Iinclude -fopenmp -flto -MMD -MP -g $(WARNINGS)
+LDLIBS = -lm
+
+SRC_DIR = src
+BUILD_DIR = build
+BIN_DIR = bin
+INPUT_DIR = input
+OUTPUT_DIR = output
+OUTPUT_IMAGE = output-image
+
+TARGET = $(BIN_DIR)/exa_beam
+
+SRCS = $(wildcard $(SRC_DIR)/*.cpp)
+
+OBJS = $(patsubst $(SRC_DIR)/%.cpp, $(BUILD_DIR)/%.o, $(SRCS))
+
+all: output-dirs $(TARGET)
+
+fast: OPT_FLAG = -Ofast
+fast: all
+
+$(TARGET): $(OBJS) | $(BIN_DIR)
+	@$(CC) $(CFLAGS) -o $@ $^ $(LDLIBS)
+	$(info Linked $@.)
+
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp | $(BUILD_DIR)
+	@$(CC) $(CFLAGS) -c $< -o $@
+	$(info Compiled $@.)
+
+$(BIN_DIR) $(BUILD_DIR):
+	@mkdir -p $@
+	$(info Created $@ directory.)
+
+output-dirs:
+	@mkdir -p $(OUTPUT_DIR) $(OUTPUT_IMAGE) $(INPUT_DIR)
+	$(info Created output directories.)
+
+clean:
+	@rm -rf $(BUILD_DIR) $(BIN_DIR) $(OUTPUT_DIR) $(OUTPUT_IMAGE) $(INPUT_DIR)
+	$(info Removed output directories.)
+
+-include $(OBJS:.o=.d)
+
+.PHONY: all clean output_dirs fast
---
+++ include/sim_structs.hpp
@@ -0,0 +1,49 @@
+/* MIT License
+
+Copyright (c) 2026 Banu Darius-Matei
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+#ifndef SIM_STRUCTS_H
+#define SIM_STRUCTS_H
+
+#include <omp.h>
+#include <memory>
+
+template <typename T>
+struct Particles {
+	int num_x, num_y, num_z;
+	std::unique_ptr<T[]> x, y, z, px, py, pz;
+	Particles(int nx, int ny, int nz) : num_x(nx), num_y(ny), num_z(nz) {
+		int total = num_x * num_y * num_z;
+		x = std::unique_ptr<T[]>(new T[num_z * num_y * num_z]);
+		y = std::unique_ptr<T[]>(new T[num_z * num_y * num_z]);
+		z = std::unique_ptr<T[]>(new T[num_z * num_y * num_z]);
+		px = std::unique_ptr<T[]>(new T[num_z * num_y * num_z]);
+		py = std::unique_ptr<T[]>(new T[num_z * num_y * num_z]);
+		pz = std::unique_ptr<T[]>(new T[num_z * num_y * num_z]);
+		#pragma omp parallel for simd schedule(static)
+		for(int i = 0; i < num_x * num_y; i++) {
+			x[i] = T(0.0); y[i] = T(0.0); z[i] = T(0.0);
+			px[i] = T(0.0); py[i] = T(0.0); pz[i] = T(0.0);
+		}
+	}
+}
+
+#endif
